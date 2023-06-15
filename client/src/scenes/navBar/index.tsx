e899914195897c279004081b38a1d83f1a9a47a2
coazz <aco94@hotmail.com>
2023-06-15 21:59:14 +0200

    Revert "deleted"
    This reverts commit a6c8c97f9730ac2dba104f2fecc1cf03bcfbbdc8.

--- client/src/scenes/navBar/index.tsx
+++ client/src/scenes/navBar/index.tsx
@@ -18,14 +18,11 @@ import {
 } from "@mui/material"
 import { Link } from "react-router-dom"
 
-const pages = ["Home", "Products", "Contact Us"]
-
 const pageLinks = [
     { link: "/", page: "Home" },
     { link: "/products", page: "Products" },
     { link: "/contact-info", page: "Contact" },
 ]
-// const settings = ["Account", "Logout"]
 
 function Navbar() {
     const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
@@ -35,7 +32,7 @@ function Navbar() {
         null
     )
 
-    const isAuth = true
+    const isAuth = false
 
     const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
         setAnchorElNav(event.currentTarget)
